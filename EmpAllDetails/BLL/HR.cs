18aefca22518836964f2a47435853c510ae6dbac
Sachin Kumar <69119583+sachinkumar83@users.noreply.github.com>
2023-05-17 19:01:46 +0530

    Add files via upload

---
+++ EmpAllDetails/BLL/HR.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+//using EmpAllDetails.BLL;
+
+namespace EmpAllDetails.BLL
+{
+    internal class HR
+    {
+       // ArrayList empData= new ArrayList();
+        //void AddEmp(Employee e)
+        //{
+        //    empData.Add(e);
+        //}
+        public bool IsValidInt(string s)
+        {
+            int num;
+            return int.TryParse(s, out num);
+           
+        }
+        public bool IsValidString(string s)
+        {
+            string pattern = @"^[A-Za-z\s]+$";
+
+            Regex regex = new Regex(pattern);
+
+            return regex.IsMatch(s);
+        }
+        public void DisplayDetails()
+        {
+            //Employee e1 = new Employee(101, "sam", "cse", 21);
+            //Employee e2 = new Employee(102, "sam", "cse", 21);
+            //Employee e3 = new Employee(103, "sam", "cse", 21);
+            //Employee e4 = new Employee(104, "sam", "cse", 21);
+            //AddEmp(e1);
+            //AddEmp(e2);
+            //AddEmp(e3);
+            //AddEmp(e4);
+            //foreach (var e in empData) { Console.WriteLine(e); }
+
+            Employee e1 = new Employee();
+            Console.WriteLine("\n\t\tEnter Employee Id");
+            e1.EmpId = Console.ReadLine();
+            Console.WriteLine("\n\t\tEnter Employee Name");
+            e1.EmpName = Console.ReadLine();
+            Console.WriteLine("\n\t\tEnter Employee Age");
+            e1.age = Console.ReadLine();
+
+            Console.WriteLine($"\n\t\tEmployee id={e1.EmpId}, \n\t\tEmployee Name ={e1.EmpName},\n\t\tEmployee age={e1.age}");
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ EmpAllDetails/Model/Employee.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmpAllDetails.BLL {
+
+    internal class Employee
+    {
+        // this class containg all private State -->encapsulate in unit or wrapping the data via properties;
+        string _EmpId;
+        string _EmpName;
+        string _EmpType;
+        string _age;
+        public string EmpId
+        {
+
+            get { return _EmpId; }
+            set
+            {
+                HR hR = new HR();
+                if (hR.IsValidInt(value)) { _EmpId = value; }
+                else
+                {
+                    bool check = true;
+                    while (check)
+                    {   
+                        Console.WriteLine("\n\t\tPlease Enter Valid No. ");
+                        string str = Console.ReadLine();
+                        if (hR.IsValidInt(str)) { _EmpId = str; check = false; }
+
+                    }
+                }
+            }
+        }
+
+        // age........
+        public string age
+        {
+
+            get { return _age; }
+            set
+            {
+                HR hR = new HR();
+                if (hR.IsValidInt(value)) { _age = value; }
+                else
+                {
+                    bool check = true;
+                    while (check)
+                    {
+                        
+                        Console.WriteLine("\n\t\tPlease Enter Valid age. ");
+                        string str = Console.ReadLine();
+                        if (hR.IsValidInt(str)) { _age = str; check = false; }
+
+                    }
+                }
+            }
+        }
+
+        // string enter name...
+        public string EmpName
+        {
+             
+            get { return _EmpName; }
+            set
+            { HR hR = new HR();
+                if (hR.IsValidString(value)) { _EmpName = value; }
+                else
+                {
+                    bool check = true;
+                    while (check)
+                    {
+
+                        Console.WriteLine("\n\t\tPlease Enter Valid Name. ");
+                        string str = Console.ReadLine();
+                        if (hR.IsValidString(str)) { _EmpName = str; check = false; }
+                    }
+                }
+            }
+        }                        
+
+
+        //public string EmpName { get; set; }
+        public string EmpType { get; set; }
+        //public int age { get; set; } // this are properties / auto generated properties...
+
+        public Employee() { }
+        //public Employee(string EmpId, string EmpName, string Emptype, int age)
+        //{
+        //    this.EmpName = EmpName;
+        //    this.EmpType = EmpType;
+        //    this.age = age;
+        //    this.EmpId = EmpId;
+        //}
+
+        //static void Main(string[] args){}
+    } }
